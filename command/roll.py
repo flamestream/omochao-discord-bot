635e34774e8ec23bb23018d80b805bf81dbd4d5c
Nicholas Souphandavong <flamestream@gmail.com>
2018-05-08 02:52:30 -0400

    Add support for roll command.

---
+++ command/roll.py
@@ -0,0 +1,38 @@
+import os
+import re
+import random
+
+async def execute(self, message, arg):
+
+	user = message.author
+
+	minimum = 1
+	maximum = 100
+
+	error = None
+	if arg:
+		matchObj = re.match( r'((\d+)(-|\s))?(\d+)', arg, re.M)
+		if matchObj:
+			if matchObj.group(4):
+				maximum = int(matchObj.group(4))
+			if matchObj.group(2):
+				minimum = int(matchObj.group(2))
+			if maximum < minimum:
+				error = True
+		else:
+			error = True
+
+	if error:
+		await self.client.reply(message, 'Argument `%s` is not valid-chao...' % arg)
+		await self.client.add_reaction_warn(message)
+	else:
+		roll = random.randint(minimum, maximum)
+		await self.client.reply(message, 'Rolled {0} ({1}-{2})'.format(roll, minimum, maximum))
+		await self.client.add_reaction_ack(message)
+
+help_message = '''Rolls a dice. The default is 1-100.
+
+Example usage:
+    -chao {0}
+    -chao {0} 6
+    -chao {0} 10-20'''.format(os.path.splitext(os.path.basename(__file__))[0])
